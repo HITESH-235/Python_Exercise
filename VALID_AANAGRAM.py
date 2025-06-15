fc8ef0f34ca6711d591a625dc82a874dc1279366
HîTESH SîNHA <codonstream.72@gmail.com>
2025-06-15 23:42:30 +0530

    Create VALID_ANAGRAM.py

---
+++ VALID_AANAGRAM.py
@@ -0,0 +1,41 @@
+#VALID_ANAGRAM: Given two strings s and t, return true if t is an anagram of s, and false otherwise. 
+#LEETCODE_link: https://leetcode.com/problems/valid-anagram/description/
+
+#Given two strings s and t, return true if t is an anagram of s, and false otherwise.
+
+def method1(a,b):
+    t = list(a)
+    u = list(b)
+    t.sort()
+    u.sort()
+    if (t == u):
+        return "True"
+    else:
+        return "False"
+
+from collections import Counter
+#counter is used to store repititions of character of a string or list into a dictionary:
+def method2(a,b):
+    return Counter(a) == Counter(b)
+
+#dictionary method (manually)
+def method3(a,b):
+    if len(a) != len(b):    #For cases where string are unequal, directly give false
+        return False
+    count = {}
+    for char in a:
+        count[char] = count.get(char,0)+1 #get doesnt add key if DNE but returns value
+
+    for char in b:
+        if char not in count:
+            return False
+        count[char] -= 1 #decrese count each time same char found in t
+        if count[char] < 0:#if count goes -ve means that t had more of that char than s
+            return False
+    return True
+
+s = "anagram"
+t = "naagram"
+print(method1(s,t))
+print(method2(s,t))
+print(method3(s,t))

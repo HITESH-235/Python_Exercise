ae91e7410ebe2433f5b55b9e872bdc6e9933b2ac
HîTESH SîNHA <codonstream.72@gmail.com>
2025-06-16 18:44:22 +0530

    Add files via upload

---
+++ Group_Anagram.py
@@ -0,0 +1,18 @@
+#GROUP ANAGRAM: Given an array of strings strs, group the anagrams together. You can return the answer in any order.
+#LEETCODE link: https://leetcode.com/problems/group-anagrams/description/
+
+from collections import defaultdict
+
+def group_anagram(lst):
+    dct = defaultdict(list)
+    for str in lst:
+        char = [0]*26
+        for x in str:
+            char[ord(x)-ord("a")] += 1  #e.g. for word aabcc: [2,1,2,0,0,0,0,0..........26 elements]
+        dct[tuple(char)].append(str)   #the words with the same key gets appended in an empty list (as value)
+        #tuple is there because list are not allowed as keys
+    return dct.values() 
+
+strs = ["act","pots","tops","cat","stop","hat"]
+
+print(group_anagram(strs))

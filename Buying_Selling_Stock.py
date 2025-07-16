75a49e67b782bab4b3ffe675a48b43eeff5dec54
HITESH-235 <codonstream.72@gmail.com>
2025-07-16 21:08:18 +0530

    Updated Buying_Selling_Stock.py

--- Buying_Selling_Stock.py
+++ Buying_Selling_Stock.py
@@ -2,73 +2,7 @@
 # Return 0 in case no profit can be made by selling
 # LEETCODE link: https://leetcode.com/problems/best-time-to-buy-and-sell-stock/
 
-lst = [56, 34, 98, 2, 45, 78, 67, 23, 10, 100, 4, 80, 55, 54, 82, 51]
-
-# def method_1(lst):
-#     right = 0
-#     arr = [0 for _ in range(len(lst))]
-
-#     for x in range(len(lst),0,-1):
-
-#         if lst[x-1] > right:
-#             right = lst[x-1]
-#         arr[x-1] = right
-
-#     profit = 0
-#     for x in range(len(lst)):
-
-#         if profit < (arr[x] - lst[x]):
-#             profit = (arr[x] - lst[x])
-
-#     return profit
-# print(method_1(lst))
-
-def method_1(lst):
-    max_right = 0
-    profit = 0
-
-    for x in range(len(lst)-1, -1, -1):
-
-        if lst[x] > max_right:
-            max_right = lst[x]
-
-        profit = max(profit, max_right - lst[x])
-    return profit
-        
-print(method_1(lst))
-def method_2(lst):
-
-    profit = 0
-    for i in range(len(lst)):
-
-        for j in range(i+1,len(lst)):
-
-            if profit < lst[j]-lst[i]:
-                profit = lst[j]-lst[i]
-
-    return profit
-
-print(method_2(lst))
-
-def method_3(lst):
-    left , right = 0 , 1
-
-    profit = 0
-    while len(lst) > right:
-
-        if lst[left] < lst[right]:
-            profit = max(profit,(lst[right]-lst[left]))
-            
-        else:
-           left = right
-        right += 1
-    return profit
-
-print(method_3(lst))
-
-
 def max_profit_brute_force(prices):
-    # Time: O(n^2), Space: O(1)
     max_profit = 0
 
     for buy_day in range(len(prices)):
@@ -80,7 +14,6 @@ def max_profit_brute_force(prices):
 
 
 def max_profit_sliding_window(prices):
-    # Time: O(n), Space: O(1)
     buy_day = 0
     sell_day = 1
     max_profit = 0
@@ -97,7 +30,6 @@ def max_profit_sliding_window(prices):
 
 
 def max_profit_min_price(prices):
-    # Time: O(n), Space: O(1)
     min_price = float('inf') #infinity representation
     max_profit = 0
 
@@ -110,7 +42,6 @@ def max_profit_min_price(prices):
 
 
 def max_profit_right_max_array(prices):
-    # Time: O(n), Space: O(n)
     n = len(prices)
     max_future_price = 0
     max_future_price_from_right = [0 for _ in range(n)]
@@ -128,7 +59,6 @@ def max_profit_right_max_array(prices):
 
 
 def max_profit_reverse_traversal(prices):
-    # Time: O(n), Space: O(1)
     max_future_price = 0
     max_profit = 0
 
@@ -140,29 +70,30 @@ def max_profit_reverse_traversal(prices):
     return max_profit
 
 
+# Example Usage:
 prices = [56, 34, 98, 2, 45, 78, 67, 23, 10, 100, 4, 80, 55, 54, 82, 51]
 import time
 start_time = time.time()
-print("Brute Force:", max_profit_brute_force(prices))
+print("Brute Force:", max_profit_brute_force(prices)) # Time: O(n^2), Space: O(1)
 end_time = time.time()
 print("1:", end_time - start_time, "seconds\n")
 
 start_time = time.time()
-print("Sliding Window:", max_profit_sliding_window(prices))
+print("Sliding Window:", max_profit_sliding_window(prices)) # Time: O(n), Space: O(1)
 end_time = time.time()
 print("2:", end_time - start_time, "seconds\n")
 
 start_time = time.time()
-print("Min Price Tracking:", max_profit_min_price(prices))
+print("Min Price Tracking:", max_profit_min_price(prices)) # Time: O(n), Space: O(1)
 end_time = time.time()
 print("3:", end_time - start_time, "seconds\n")
 
 start_time = time.time()
-print("Right Max Array:", max_profit_right_max_array(prices))
+print("Right Max Array:", max_profit_right_max_array(prices)) # Time: O(n), Space: O(n)
 end_time = time.time()
 print("4:", end_time - start_time, "seconds\n")
 
 start_time = time.time()
-print("Reverse Traversal:", max_profit_reverse_traversal(prices))
+print("Reverse Traversal:", max_profit_reverse_traversal(prices)) # Time: O(n), Space: O(1)
 end_time = time.time()
 print("5:", end_time - start_time, "seconds\n")

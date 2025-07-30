f113bc55479e912714419a9f2bf898e15606ec6f
HITESH-235 <codonstream.72@gmail.com>
2025-07-31 04:07:45 +0530

    Updated Min Stack

--- Stack/Min_Stack.py
+++ Stack/Min_Stack.py
@@ -30,11 +30,48 @@ class MinStack:
             return
         self.top = self.top.next
 
-    def getmin(self):
+    def getMin(self):
         return self.top.cur_min
 
     def peek(self):
         return self.top.data
+
+
+class StackNode_2:
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+        
+class MinStack_2:
+    def __init__(self):
+        self.top = None
+        self.cur_min = None
+
+    def is_empty(self):
+        return self.top is None
+
+    def push(self,data):
+        if self.is_empty():
+            self.top = StackNode_2(0)
+            self.cur_min = data
+            return
+        new_node = StackNode_2(data - self.cur_min)
+        new_node.next = self.top
+        self.top = new_node
+        self.cur_min = min(self.cur_min, data)
+
+    def pop(self):
+        if self.is_empty():
+            return
+        if self.top.data < 0:
+            self.min = self.cur_min - self.top.data
+        self.top = self.top.next
+
+    def getMin(self):
+        return self.cur_min
+
+    def peek(self):
+        return self.top.data + self.cur_min
 
 
 obj = MinStack()
@@ -48,5 +85,18 @@ obj.push(-4)
 obj.push(8)
 obj.pop()
 obj.push(5)
-print(obj.getmin())
+print(obj.getMin())
+print(obj.peek())
+obj = MinStack_2()
+obj.push(10)
+obj.push(2)
+obj.push(-1)
+obj.pop()
+obj.push(3)
+obj.push(6)
+obj.push(-4)
+obj.push(8)
+obj.pop()
+obj.push(5)
+print(obj.getMin())
 print(obj.peek())

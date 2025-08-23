b8a5136d55c57db2f407da74e0c0830b8ace0bb2
HITESH-235 <codonstream.72@gmail.com>
2025-08-23 17:51:14 +0530

    23 Aug 2025

--- Stack/NextGreaterElement_I.py
+++ Stack/NextGreaterElement_I.py
@@ -1,37 +1,61 @@
 # The next greater element of some element x in an array is the first greater element that is to the right of x in the same array.
 # LEETCODE link: https://leetcode.com/problems/next-greater-element-i/description/
 
-def brute_force(nums):
-    res = []
-    n = len(nums)
+
+def brute_force(nums1,nums2):
+    temp = {}
+    n = len(nums2)
     for i in range(n-1):
         j = i+1
         
         while j < n:
-            if nums[j] > nums[i]:
-                res.append(nums[j])
+            if nums2[j] > nums2[i]:
+                temp[nums2[i]] = nums2[j]
                 j = n
             elif j == n-1:
-                res.append(-1)
+                temp[nums2[i]] = (-1)
                 j = n
             j+=1
-    res.append(-1)
+    temp[nums2[n-1]] = -1
+    res = []
+    for num in nums1:
+        res.append(temp[num])
     return res
 
 
-def stack_(nums):
-    n = len(nums)
-    st = [nums[-1]]
-    res = [-1 for _ in range(n)]
-    for i in range(n-2,-1,-1):
-        while st and nums[i] >= st[-1]:
+def stack_2(nums1, nums2):
+    n2 = len(nums2)
+    st = [nums2[-1]]
+    temp = [-1 for _ in range(n2)]
+    for i in range(n2-2,-1,-1):
+        while st and nums2[i] >= st[-1]:
             st.pop()
         if st:
-            res[i] = st[-1]
-        st.append(nums[i])
+            temp[i] = st[-1]
+        st.append(nums2[i])
+    res = []
+    for num in nums1:
+        res.append(temp[nums2.index(num)])
     return res
 
 
-nums = [6,10,8,3,4,5,5,2,6,2,84,74,2,6,7,9,10,56,3,4,6,7,8,4,5,6,3,6,4,5,456,6,3,6,4,334,6,7,433,6,3,6,34,34,34]
-print(brute_force(nums))
-print(stack_(nums))
+def optimised(nums1, nums2):
+    st = []
+    map = {}
+    for num in nums2:
+        while st and st[-1] < num:
+            smaller = st.pop()
+            map[smaller] = num
+        st.append(num)
+    for num in st:
+        map[num] = -1
+    res = []
+    for num in nums1:
+        res.append(map[num])
+    return res
+
+nums1 = [2, 7, 13, 25, 30, 45, 60, 90, 120, 150]
+nums2 = [2, 1, 7, 5, 13, 10, 25, 20, 30, 28, 45, 40, 60, 55, 90, 85, 120, 110, 150, 200]
+print("Brute force:",brute_force(nums1,nums2))
+print("Stack:",stack_2(nums1,nums2))
+print("Stack Optimised:",optimised(nums1, nums2))
---
+++ Stack/NextGreaterElement_II.py
@@ -0,0 +1,2 @@
+# Given a circular integer array nums (i.e., the next element of nums[nums.length - 1] is nums[0]), return the next greater number for every element in nums.
+# LEETCODE link: https://leetcode.com/problems/next-greater-element-ii/description/

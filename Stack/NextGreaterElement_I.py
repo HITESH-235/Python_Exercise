f8148e230bfa2ce0c9dfaef59253454acd6457dd
HITESH-235 <codonstream.72@gmail.com>
2025-08-23 15:31:06 +0530

    23 Aug 2025

--- Stack/NextGreaterElement_I.py
+++ Stack/NextGreaterElement_I.py
@@ -24,13 +24,14 @@ def stack_(nums):
     st = [nums[-1]]
     res = [-1 for _ in range(n)]
     for i in range(n-2,-1,-1):
-        while st!=[] and nums[i] >= st[-1]:
+        while st and nums[i] >= st[-1]:
             st.pop()
         if st:
             res[i] = st[-1]
         st.append(nums[i])
     return res
-    
+
+
 nums = [6,10,8,3,4,5,5,2,6,2,84,74,2,6,7,9,10,56,3,4,6,7,8,4,5,6,3,6,4,5,456,6,3,6,4,334,6,7,433,6,3,6,34,34,34]
 print(brute_force(nums))
 print(stack_(nums))

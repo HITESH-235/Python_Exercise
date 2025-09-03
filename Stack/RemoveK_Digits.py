6ce42e57cd4d92e69148d2c3e4ea4eddf6f7b798
HITESH-235 <codonstream.72@gmail.com>
2025-09-03 13:37:19 +0530

    3 Sep 2025

--- Stack/RemoveK_Digits.py
+++ Stack/RemoveK_Digits.py
@@ -3,14 +3,14 @@
 
 def brute_force(num,k):
     if k == 0:
-        return num.lstrip("0") if int(num) != 0 else "0"
-    if len(num) <= k:
+        return num.lstrip("0") if int(num) != 0 else "0" # trims of left 0s every time called
+    if len(num) <= k: # for cases like this every digit is lost
         return "0"
     
     least = None
     for i in range(len(num)):
-        curr = brute_force((num[:i]+num[i+1:]),k-1)
-        if least is None or int(least) > int(curr):
+        curr = brute_force((num[:i]+num[i+1:]),k-1) # checks and remove every index till num is None
+        if least is None or int(least) > int(curr): # updates least with min value of current num
             least = curr
     return least
 
@@ -20,26 +20,26 @@ def stack_(num, k):
     if n <= k:
         return "0"
 
-    st = []
+    st = [] # will contain every digit except the smaller digits that come
     for i in range(n):
         while st and k>0 and ord(st[-1])>ord(num[i]):
             st.pop()
             k -= 1
-        st.append(num[i])
+        st.append(num[i]) # only digits greater than st[-1] gets appended
 
-    while k > 0:
+    while k > 0: # hence if we remove the last digits in st we are removing the greatest digits currently in st
         st.pop()
         k -= 1
-    if not st:
+    if not st: # we might* remove every element
         return "0"
 
     res = ""
-    flag = 0
+    flag = 0 # using flag to ignore left 0s
     for item in st:
-        if flag == 0 and item == "0":
+        if flag == 0 and item == "0": # if flag still not flipped and item 0 then it is a left 0 
             continue
         else:
-            flag = 1
+            flag = 1 # if flag flipped then some other number came and now left 0s are finished
             res += item
 
     return res if res else "0"
